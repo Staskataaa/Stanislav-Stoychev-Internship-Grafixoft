95dda19ce14d5ad1422980b9dca4adc620d01253
Stanislav Stoychev <stanislav.stoychev@grafixoft.com>
2022-07-26 14:48:11 +0300

    Chapter 7 special Charaters

--- Chapter 07/Special Characters/Special Characters/Program.cs
+++ Chapter 07/Special Characters/Special Characters/Program.cs
@@ -11,132 +11,42 @@ namespace Special_Characters
         public static List<int> ListOfSpecialNumbers(int num)
         {
             List<int> specialNumbers = new List<int>();
-            List<int> divisibleNumbers = new List<int>();
-            for (int i = 1; i < num + 1; i++)
+            List<int> totalNumbers = new List<int>();
+            for (int i = 1111; i < 10000; i++)
             {
-                if (num < 10000)
+                if (!i.ToString().Contains("0"))
                 {
-                    if (num % i == 0)
-                    {
-                        divisibleNumbers.Add(i);
-                    }
+                    totalNumbers.Add(i);
                 }
-                else
-                    break;
             }
-            for (int a = 0; a < divisibleNumbers.Count(); a++)
+            for (int a = 0; a < totalNumbers.Count(); a++)
             {
-                int toBeAdded = 0;
-                if (divisibleNumbers[a] > 0 && divisibleNumbers[a] <= 9)
+                int checkOne = totalNumbers.ElementAt(a) / 1000;
+                int checkTwo = (totalNumbers.ElementAt(a) / 100) % 10;
+                int checkThree = (totalNumbers.ElementAt(a) / 10) % 10;
+                int checkFour = totalNumbers.ElementAt(a) % 10;
+                if (num % checkOne == 0)
                 {
-                    toBeAdded = divisibleNumbers[a];
-                    for (int b = 0; b < divisibleNumbers.Count(); b++)
+                    if (num % checkTwo == 0)
                     {
-                        if (divisibleNumbers[b] >= 0 && divisibleNumbers[b] <= 9)
+                        if (num % checkThree == 0)
                         {
-                            int twoDigits = (toBeAdded * 10) + divisibleNumbers[b];
-                            for (int c = 0; c < divisibleNumbers.Count(); c++)
+                            if (num % checkFour == 0)
                             {
-                                if (divisibleNumbers[c] >= 0 && divisibleNumbers[c] <= 9)
-                                {
-                                    int threeDigits = (twoDigits * 10) + divisibleNumbers[c];
-                                    for (int e = 0; e < divisibleNumbers.Count(); e++)
-                                    {
-                                        if (divisibleNumbers[e] >= 0 && divisibleNumbers[e] <= 9)
-                                        {
-                                            int fourDigits = (threeDigits * 10) + divisibleNumbers[e];
-                                            specialNumbers.Add(fourDigits);
-                                        }
-                                        else
-                                            break;
-                                    }
-                                }
-                                else if (divisibleNumbers[c] >= 10 && divisibleNumbers[c] <= 99)
-                                {
-                                    int fourDigits = (twoDigits * 100) + divisibleNumbers[c];
-                                    specialNumbers.Add(fourDigits);
-                                }
-                                else
-                                    break;
+                                specialNumbers.Add(totalNumbers[a]);
                             }
                         }
-                        else if (divisibleNumbers[b] >= 10 && divisibleNumbers[b] <= 99)
-                        {
-                            int threeDigits = (toBeAdded * 100) + divisibleNumbers[b];
-                            for (int d = 0; d < divisibleNumbers.Count(); d++)
-                            {
-                                if (divisibleNumbers[d] >= 0 && divisibleNumbers[d] <= 9)
-                                {
-                                    int fourDigits = (threeDigits * 10) + divisibleNumbers[d];
-                                    specialNumbers.Add(fourDigits);
-                                }
-                            }
-                        }
-                        else if (divisibleNumbers[b] >= 100 && divisibleNumbers[b] <= 999)
-                        {
-                            int fourDigits = (toBeAdded * 1000) + divisibleNumbers[b];
-                            specialNumbers.Add(fourDigits);
-                        }
-                        else
-                            break;
                     }
                 }
-                else if (divisibleNumbers[a] >= 10 && divisibleNumbers[a] <= 99)
-                {
-                    int twoDigits = divisibleNumbers[a];
-                    for (int b = 0; b < divisibleNumbers.Count(); b++)
-                    {
-                        if (divisibleNumbers[b] >= 0 && divisibleNumbers[b] <= 9)
-                        {
-                            int threeDigit = (twoDigits * 10) + divisibleNumbers[b];
-                            for (int d = 0; d < divisibleNumbers.Count(); d++)
-                            {
-                                if (divisibleNumbers[d] >= 0 && divisibleNumbers[d] <= 9)
-                                {
-                                    int fourDigit = (threeDigit * 10) + divisibleNumbers[d];
-                                    specialNumbers.Add(fourDigit);
-                                }
-                                else
-                                    break;
-                            }
-                        }
-                        else if (divisibleNumbers[b] >= 10 && divisibleNumbers[b] <= 99)
-                        {
-                            int fourDigit = (twoDigits * 100) + divisibleNumbers[b];
-                            specialNumbers.Add(fourDigit);
-                        }
-                        else
-                            break;
-                    }
-                }
-                else if (divisibleNumbers[a] >= 100 && divisibleNumbers[a] <= 999)
-                {
-                    int threeDigit = divisibleNumbers[a];
-                    for (int c = 0; c < divisibleNumbers.Count(); c++)
-                    {
-                        if (divisibleNumbers[c] >= 0 && divisibleNumbers[c] <= 9)
-                        {
-                            int fourDigit = (threeDigit * 10) + divisibleNumbers[c];
-                            specialNumbers.Add(fourDigit);
-                        }
-                        else
-                            break;
-                    }
-                }
-                else if (divisibleNumbers[a] >= 1000 && divisibleNumbers[a] <= 9999)
-                {
-                    specialNumbers.Add(divisibleNumbers[a]);
-                }
-                else
-                    break;
             }
+
             return specialNumbers;
         }
 
         static void Main(string[] args)
         {
             int num = int.Parse(Console.ReadLine());
-            List<int> listNum = ListOfSpecialNumbers(num).Distinct().ToList();
+            List<int> listNum = ListOfSpecialNumbers(num);
             foreach (int specialNum in listNum)
             {
                 Console.Write("{0} ", specialNum);
---
+++ Chapter 08/Bombs/Bombs/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bombs
+{
+    internal class Program
+    {
+        public static int[,] DetonateBombs(int[][] jaggedArray, int[,] matrix)
+        {
+            foreach (int[] array in jaggedArray)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if ((array[0] == i && array[1] == j) && matrix[i, j] != 0)
+                        {
+                            int bomb = matrix[i, j];
+                            matrix[i, j] = 0;
+                            if (i != 0)
+                            {
+                                
+                            }
+                        }
+                    }
+                }
+            }
+            return matrix;
+        }
+
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] row = Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = row[j];
+                }
+            }
+            int[][] bombSpotsJaggedArray = Console.ReadLine().Split(' ').Select(s => s.Split(',').Select(f => int.Parse(f)).ToArray()).ToArray();
+            foreach (int[] array in bombSpotsJaggedArray)
+            {
+                if (array[0] < matrix.GetLength(0) && array[1] < matrix.GetLength(1))
+                {
+                    sum += matrix[array[0], array[1]];
+                }
+            }
+            Console.WriteLine("Alive cells: {0}", bombSpotsJaggedArray.GetLength(0));
+            Console.WriteLine("Sum: {0}", sum);
+            matrix = DetonateBombs(bombSpotsJaggedArray ,matrix);
+            for(int i = 0; i < matrix.GetLength(0); i++)
+
+            Console.ReadLine();
+            
+        }
+    }
+}
