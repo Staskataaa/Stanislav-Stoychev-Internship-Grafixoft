6f7897ccce72a0c07048cfe00e532ba068a61ba3
Stanislav Stoychev <stanislav.stoychev@grafixoft.com>
2022-07-27 13:47:09 +0300

    Chapter 8 Bombs

--- Chapter 08/Bombs/Bombs/Program.cs
+++ Chapter 08/Bombs/Bombs/Program.cs
@@ -8,87 +8,66 @@ namespace Bombs
 {
     internal class Program
     {
-        public static void CalculateAndPrintSum(int[,] matrix)
+        public static void CalculateAndPrintSum(ref int[,] matrix)
         {
+            int aliveCells = 0;
             int sum = 0;
+            string arrayPrint = "";
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 for (int j = 0; j < matrix.GetLength(1); j++)
                 {
+                    arrayPrint += matrix[i, j].ToString() + " ";
                     if (matrix[i, j] > 0)
-                    {
+                    {                      
                         sum += matrix[i, j];
+                        aliveCells++;
                     }
                 }
+                arrayPrint += "\n";
             }
+            Console.WriteLine("Alive cells: {0}", aliveCells);
             Console.WriteLine("Sum: {0}", sum);
+            Console.WriteLine(arrayPrint);
         }
 
-        public static bool CheckAdjacentElement(int i, int j, int n, int m)
+        public static void DetonateOneBomb(int rowIndex, int colIndex, ref int[,] matrix)
         {
-            if (i < 0 || j < 0 || i > n - 1 || j > m - 1)
-                return false;
-            return true;
-        }
+            int bombValue = matrix[rowIndex, colIndex];
 
-        public static int[,] DetonateBombs(int[][] jaggedArray, int[,] matrix)
-        {
-            foreach (int[] array in jaggedArray)
+            for (int i = rowIndex - 1; i < rowIndex + 2; i++)
             {
-                for (int i = 0; i < matrix.GetLength(0); i++)
+                for (int j = colIndex - 1; j < colIndex + 2; j++)
                 {
-                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    if (i >= 0 && j >= 0 && i < matrix.GetLength(0) && (j < matrix.GetLength(1)))
                     {
-                        if ((array[0] == i && array[1] == j) && matrix[i, j] != 0)
+                        if (matrix[i, j] > 0)
                         {
-                            int bomb = matrix[i, j];
-                            matrix[i, j] = 0;
-                            int n = matrix.GetLength(0);
-                            int m = matrix.GetLength(1);
-                            if (CheckAdjacentElement(i - 1, j - 1, n, m) == true && matrix[i - 1, j - 1] > 0) 
-                            {
-                                matrix[i - 1, j - 1] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i, j - 1, n, m) == true && matrix[i, j - 1] > 0)
-                            {
-                                matrix[i, j - 1] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i - 1, j, n, m) == true && matrix[i - 1, j] > 0)
-                            {
-                                matrix[i - 1, j] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i - 1, j + 1, n, m) == true && matrix[i - 1, j + 1] > 0)
-                            {
-                                matrix[i - 1, j + 1] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i, j + 1, n, m) == true && matrix[i, j + 1] > 0)
-                            {
-                                matrix[i, j + 1] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i + 1, j, n, m) == true && matrix[i + 1, j] > 0)
-                            {
-                                matrix[i + 1, j] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i + 1, j + 1, n, m) == true && matrix[i + 1, j + 1] > 0)
-                            {
-                                matrix[i + 1, j + 1] -= bomb;
-                            }
-                            if (CheckAdjacentElement(i + 1, j - 1, n, m) == true && matrix[i + 1, j - 1] > 0)
-                            {
-                                matrix[i + 1, j - 1] -= bomb;
-                            }
+                            matrix[i, j] -= bombValue;
                         }
                     }
                 }
             }
-            CalculateAndPrintSum(matrix);
+        }
+
+        public static int[,] DetonateAllBombs(int[][] jaggedArray, ref int[,] matrix)
+        {
+            foreach (int[] array in jaggedArray)
+            {
+                int rowCoordianate = array[0];
+                int colCoordinate = array[1];
+                DetonateOneBomb(rowCoordianate, colCoordinate, ref matrix);
+            }
+
+            CalculateAndPrintSum(ref matrix);
             return matrix;
         }
 
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            int[,] matrix = new int[n,n];
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize,matrixSize];
+
             for (int i = 0; i < matrix.GetLength(0); i++)
             {
                 int[] row = Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
@@ -97,22 +76,9 @@ namespace Bombs
                     matrix[i, j] = row[j];
                 }
             }
+
             int[][] bombSpotsJaggedArray = Console.ReadLine().Split(' ').Select(s => s.Split(',').Select(f => int.Parse(f)).ToArray()).ToArray();
-            Console.WriteLine("Alive cells: {0}", bombSpotsJaggedArray.GetLength(0));
-            matrix = DetonateBombs(bombSpotsJaggedArray ,matrix);
-            for (int i = 0; i < matrix.GetLength(0); i++)
-            {
-                Console.WriteLine();
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                    if (j == matrix.GetLength(1) - 1)
-                    {
-                        Console.Write("{0}", matrix[i, j]);
-                        break;
-                    }
-                    Console.Write("{0} ", matrix[i, j]);
-                }
-            }
+            DetonateAllBombs(bombSpotsJaggedArray , ref matrix);
             Console.ReadLine();
             
         }
