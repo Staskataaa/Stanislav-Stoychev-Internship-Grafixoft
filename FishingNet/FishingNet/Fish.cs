b91bf5b34673123d35f8a9340a9ef2cbfd9200fa
Stanislav Stoychev <stanislav.stoychev@grafixoft.com>
2022-07-28 17:40:21 +0300

    FishingNet unfinished

---
+++ FishingNet/FishingNet/Fish.cs
@@ -0,0 +1,40 @@
+﻿namespace FishingNet
+{
+    public class Fish
+    {
+        private string _fishType;
+
+        private double _length;
+
+        private double _weight;
+
+        public string FishType
+        {
+            get { return _fishType; }
+            set { _fishType = value; }
+        }
+
+        public double Length
+        {
+            get { return _length; }
+            set { _length = value; }
+        }
+        public double Weigth
+        {
+            get { return _weight; }
+            set { _weight = value; }
+        }
+
+        public Fish(string fishType, double length, double weight)
+        {
+            _fishType = fishType;
+            _length = length;
+            _weight = weight;
+        }
+
+        public override string ToString()
+        { 
+            return "There is a " + _fishType + ", " + _length + " cm. long and " + _weight + " gr. in weight";
+        }
+    }
+}
---
+++ FishingNet/FishingNet/Net.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FishingNet
+{
+    public class Net 
+    {
+        private static int _capacity;
+        private string _material;
+        private List<Fish> _Fish;
+
+        public List<Fish> Fish
+        { get; set; }
+
+        public int capacity
+        {
+            get { return _capacity; }
+            set { _capacity = value; }
+        }
+
+        public string material
+        {
+            get { return _material; }
+            set { _material = value; }
+        }
+
+        public int Count
+        {
+            get 
+            {
+                int count = 0;
+                for (int i = 0; i < _Fish.Count; i++)
+                {
+                    if (_Fish[i] != null)
+                    {
+                        count++;
+                    }    
+                }
+                return count;
+            }
+        }
+
+        public Net(string material, int capacity)
+        {
+            this.capacity = capacity;
+            this.material = material;
+            _Fish = new List<Fish>(capacity);
+        }
+
+        public string AddFish(Fish fish)
+        {
+            string result = "";
+            bool IsAdded = false;
+            if (IsValidFish(fish))
+            {
+                for (int i = 0; i < capacity; i++)
+                {
+                    _Fish.Add(fish);
+                }
+            }
+            else
+            {
+                result = "Invalid fish.";
+            }
+            return result;
+        }
+
+        private bool IsValidFish(Fish fish)
+        {
+            bool result = false;
+            if (fish.FishType != "" && fish.FishType != null && Math.Min(fish.Length, fish.Weigth) > 0)
+            {
+                result = true;
+            }
+            return result;
+        }
+
+    }
+}
