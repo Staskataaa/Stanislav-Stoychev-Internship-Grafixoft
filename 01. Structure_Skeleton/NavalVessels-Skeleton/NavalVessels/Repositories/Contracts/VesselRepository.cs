7314649aa13554cf74183b2631b6268dd1402283
Stanislav Stoychev <stanislav.stoychev@grafixoft.com>
2022-08-01 17:04:16 +0300

    Naval Vessels Skeleton

---
+++ 01. Structure_Skeleton/NavalVessels-Skeleton/NavalVessels/Repositories/Contracts/VesselRepository.cs
@@ -0,0 +1,34 @@
+﻿using NavalVessels.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NavalVessels.Repositories.Contracts
+{
+    internal class VesselRepository : IRepository<Vessel>
+    {
+        private List<Vessel> _models;
+
+        public IReadOnlyCollection<Vessel> Models
+        {
+            get { return _models; }
+        }
+
+        public void Add(Vessel model)
+        {
+            _models.Add(model);
+            _models.Distinct();
+        }
+
+        public Vessel FindByName(string name)
+        {
+            return _models.FirstOrDefault(x => x.Name == name);
+        }
+
+        public bool Remove(Vessel model)
+        {
+            return _models.Remove(model);
+        }
+    }
+}
---
+++ 01. Structure_Skeleton/NavalVessels-Skeleton/NavalVessels/StartUp.cs
@@ -0,0 +1,17 @@
+﻿namespace NavalVessels
+{
+    using Core;
+    using Core.Contracts;
+    using NavalVessels.Models.Entities;
+    using System;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            Engine engine = new Engine();
+            engine.Run();
+          
+        }
+    }
+}
