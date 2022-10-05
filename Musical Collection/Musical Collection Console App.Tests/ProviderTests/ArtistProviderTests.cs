7f13e6cd02fd0415b3f4c0eec49edd136cb98a8e
Staskataaa <99052852+Staskataaa@users.noreply.github.com>
2022-10-05 17:01:55 +0300

    Add Musical Collection

---
+++ Musical Collection/Musical Collection Console App.Tests/ProviderTests/ArtistProviderTests.cs
@@ -0,0 +1,103 @@
+﻿using Moq;
+using Musical_Collection_Console_App.Providers;
+using Musical_Collection_Console_App.Utils.Exception_Messages;
+using Musical_Collection_Console_App.Utils.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musical_Collection_Console_App.Tests.ProviderTests
+{
+    public class ArtistProviderTests
+    {
+        private ArtistProvider artistProvider;
+        private Artist artist;
+        private Mock<EntityRepository<Artist>> mockArtistRepo;
+
+        [SetUp]
+        public void SetUp()
+        {
+            mockArtistRepo = new Mock<EntityRepository<Artist>>();
+            artistProvider = new ArtistProvider(mockArtistRepo.Object);
+            artist = new Artist("Galena", "123456", "Galina Gencheva", new DateTime(1987, 08, 15));
+            mockArtistRepo.Setup(x => x.Save(artist)).Verifiable();
+            mockArtistRepo.Setup(x => x.FindTByName(artist.Name)).Returns(artist);
+            mockArtistRepo.Setup(x => x.Update(artist)).Verifiable();
+        }
+
+        [Test]
+        public void Register_Successful()
+        {
+            //act
+            artistProvider.Register(artist);
+
+            //assert
+            mockArtistRepo.Verify(mock => mock.Save(artist), Times.Once);
+        }
+
+        [Test]
+        public void Register_Unsuccessful()
+        {
+            //asssign
+            mockArtistRepo.Setup(x => x.Save(artist)).Throws<Exception>(
+                () => throw new ArgumentException(ExceptionMessagesRepositoryMessages.IsNotUnique));
+
+            //act and assert
+            Assert.Throws<ArgumentException>(() => artistProvider.Register(artist));
+        }
+
+        [Test]
+        public void Login_Successful()
+        {
+            //act
+            artistProvider.Login(artist.Name, artist.Password);
+
+            //assert
+            mockArtistRepo.Verify(mock => mock.Update(artist), Times.Once);
+            mockArtistRepo.Verify(mock => mock.FindTByName(artist.Name), Times.Once);
+        }
+
+        [Test]
+        public void Login_Unsuccessful_AleadyLoggedIn()
+        {
+            //assign 
+            artist.IsActive = true;
+           
+            //act and assert
+            Assert.Throws<ArgumentException>(() => artistProvider.Login(artist.Name, artist.Password));
+        }
+
+        [Test]
+        public void Login_Unsuccessful_InvalidPassword()
+        {
+            //assign 
+            string password = "Invalid Password";
+
+            //act and assert
+            Assert.Throws<ArgumentException>(() => artistProvider.Login(artist.Name, password));
+        }
+
+        [Test]
+        public void Logout_Successful()
+        {
+            //assign 
+            artist.IsActive = true;
+           
+            //act
+            artistProvider.Logout(artist.Name);
+
+            //assert
+            mockArtistRepo.Verify(mock => mock.Update(artist), Times.Once);
+            mockArtistRepo.Verify(mock => mock.FindTByName(artist.Name), Times.Once);
+        }
+
+        [Test]
+        public void Logout_Unsuccessful_NotLoggedIn()
+        {
+            //act and assert
+            Assert.Throws<ArgumentException>(() => artistProvider.Logout(artist.Name));
+        }
+    }
+}
---
+++ Musical Collection/Musical Collection Console App/Classes/MusicalCollection.cs
@@ -0,0 +1,30 @@
+﻿using Musical_Collection_Console_App.Interfaces.Classes_Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Musical_Collection_Console_App.Classes
+{
+    public abstract class MusicalCollection : IMusicalCollection
+    {
+        private string _name;
+        private IEnumerable<Song> _collection;
+        private IEnumerable<string> _genres;
+
+        public MusicalCollection(string name)
+        {
+            Name = name;
+            Collection = new List<Song>();
+        }
+
+        public string Name { get; set; }
+
+        public IEnumerable<Song> Collection { get; set; }
+
+        public IEnumerable<string> Genres => Collection.Select(s => s.Genre).Distinct().ToList();
+
+        public double Duration { get; set; }
+    }
+}
